9b83b4974bbf81d2d86fdb80b3d56108a6b02a2f
Priyesh <91priyesh@gmail.com>
2019-01-01 20:44:02 -0500

    final update of 1/1/19- buttons working to recall animated gifs from Giphy

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,11 +4,8 @@ console.log(name);
 //event listerner for all button elements
 $("button").on("click", function () {
   var choice = $(this).attr("data-choice");
-});
-
+  
 var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + choice + "&api_key=hkxKxvHGZkDCWiZCAbGXUxBA5SR2U766";
-
-
 //ajax request
 $.ajax({
     url: queryURL,
@@ -16,15 +13,23 @@ $.ajax({
   })
   .then(function (response) {
     var results = response.data;
-
+    console.log("hi");
     for (var i = 0; i < results.length; i++) {
       var choiceDiv = $("<div>");
-    }
-    var p = $("<p>").text(`Rating: ${results[i].rating}`);
+    console.log(results);
+    var rating = results[i].rating
+    var p = $("<p>").text("Rating: " + rating);
+    
     var choiceImage = $("<img>");
+    
     choiceImage.attr("src", results[i].images.fixed_height.url);
+    
     choiceDiv.append(p);
+    
     choiceDiv.append(choiceImage);
+    
     $(".gifBox").prepend(choiceDiv);
-
+    }
   });
+});
+
